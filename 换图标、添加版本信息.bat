61bf93ff7ddf23b842dd18e21c1969e33067b34a
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-09-23 10:41:51 +0800

    fix: 补齐测试平台运行时图标交付流程

--- 换图标、添加版本信息.bat
+++ 换图标、添加版本信息.bat
@@ -1,3 +1,5 @@
+@echo off
+if /i "%~1"=="test" goto test_only
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV AI Platform-win_x64.exe" ^
 -action addoverwrite -res "ico.ico" -mask ICONGROUP,MAINICON,0
 
@@ -24,3 +26,31 @@ cp "dist/DLCV AI Training Platform-win_x64.exe" "..\dlcv_train\frontend\bin\neut
 cp "dist/DLCV Test-win_x64.exe" "..\dlcv_test\neu\bin\neutralino-win_x64.exe"
 
 pause
+exit /b
+
+:test_only
+setlocal
+if "%~2"=="" (
+    echo Usage: "%~nx0" test "project\neu\bin\neutralino-win_x64.exe"
+    exit /b 2
+)
+if not exist "%~dp2" exit /b 2
+pushd "%~dp0"
+if errorlevel 1 exit /b 1
+set "RESOURCE_HACKER=C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe"
+if not exist "%RESOURCE_HACKER%" goto test_failed
+if not exist "bin\neutralino-win_x64.exe" goto test_failed
+"%RESOURCE_HACKER%" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" -action addoverwrite -res "dist/test.ico" -mask ICONGROUP,MAINICON,0 -log console
+if errorlevel 1 goto test_failed
+"%RESOURCE_HACKER%" -open "dist/test.rc" -save "dist/test.res" -action compile -log console
+if errorlevel 1 goto test_failed
+"%RESOURCE_HACKER%" -open "dist/DLCV Test-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" -action addoverwrite -res "dist/test.res" -mask VERSIONINFO -log console
+if errorlevel 1 goto test_failed
+copy /y "dist\DLCV Test-win_x64.exe" "%~f2" > nul
+if errorlevel 1 goto test_failed
+popd
+exit /b 0
+
+:test_failed
+popd
+exit /b 1
