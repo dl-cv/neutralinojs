737418c02fabb1b55de8599a669758e1ef254d67
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-09-23 21:44:48 +0800

    fix: 恢复自动复制并补充 Studio 图标资源

--- 换图标、添加版本信息.bat
+++ 换图标、添加版本信息.bat
@@ -1,5 +1,3 @@
-@echo off
-if /i "%~1"=="test" goto test_only
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV AI Platform-win_x64.exe" ^
 -action addoverwrite -res "ico.ico" -mask ICONGROUP,MAINICON,0
 
@@ -8,6 +6,9 @@ if /i "%~1"=="test" goto test_only
 
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" ^
 -action addoverwrite -res "dist/test.ico" -mask ICONGROUP,MAINICON,0
+
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV AI Studio-win_x64.exe" ^
+-action addoverwrite -res "ico.ico" -mask ICONGROUP,MAINICON,0
 
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open dist/env.rc -save dist/env.res -action compile -log console
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "dist/DLCV AI Platform-win_x64.exe" -save "dist/DLCV AI Platform-win_x64.exe" ^
@@ -21,36 +22,13 @@ if /i "%~1"=="test" goto test_only
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "dist/DLCV Test-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" ^
 -action addoverwrite -res "dist/test.res" -mask VERSIONINFO
 
-cp "dist/DLCV AI Platform-win_x64.exe" "..\dlcv_env\neutralinojs\bin\neutralino-win_x64.exe"
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open dist/studio.rc -save dist/studio.res -action compile -log console
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "dist/DLCV AI Studio-win_x64.exe" -save "dist/DLCV AI Studio-win_x64.exe" ^
+-action addoverwrite -res "dist/studio.res" -mask VERSIONINFO
+
+cp "dist/DLCV AI Platform-win_x64.exe" "..\dlcv_ai_platform\neutralinojs\bin\neutralino-win_x64.exe"
+cp "dist/DLCV AI Studio-win_x64.exe" "..\ai_studio\neu\bin\neutralino-win_x64.exe"
 cp "dist/DLCV AI Training Platform-win_x64.exe" "..\dlcv_train\frontend\bin\neutralino-win_x64.exe"
 cp "dist/DLCV Test-win_x64.exe" "..\dlcv_test\neu\bin\neutralino-win_x64.exe"
 
 pause
-exit /b
-
-:test_only
-setlocal
-if "%~2"=="" (
-    echo Usage: "%~nx0" test "project\neu\bin\neutralino-win_x64.exe"
-    exit /b 2
-)
-if not exist "%~dp2" exit /b 2
-pushd "%~dp0"
-if errorlevel 1 exit /b 1
-set "RESOURCE_HACKER=C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe"
-if not exist "%RESOURCE_HACKER%" goto test_failed
-if not exist "bin\neutralino-win_x64.exe" goto test_failed
-"%RESOURCE_HACKER%" -open "bin/neutralino-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" -action addoverwrite -res "dist/test.ico" -mask ICONGROUP,MAINICON,0 -log console
-if errorlevel 1 goto test_failed
-"%RESOURCE_HACKER%" -open "dist/test.rc" -save "dist/test.res" -action compile -log console
-if errorlevel 1 goto test_failed
-"%RESOURCE_HACKER%" -open "dist/DLCV Test-win_x64.exe" -save "dist/DLCV Test-win_x64.exe" -action addoverwrite -res "dist/test.res" -mask VERSIONINFO -log console
-if errorlevel 1 goto test_failed
-copy /y "dist\DLCV Test-win_x64.exe" "%~f2" > nul
-if errorlevel 1 goto test_failed
-popd
-exit /b 0
-
-:test_failed
-popd
-exit /b 1
